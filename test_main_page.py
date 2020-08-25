9ee6a4c9136e832320d6fb754ab1656be98a156e
tbs99 <timsregisters@gmail.com>
2020-08-25 16:27:28 +0300

    working with two Page Objects

--- test_main_page.py
+++ test_main_page.py
@@ -5,9 +5,11 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 
 def test_guest_can_go_to_login_page(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
+    main_page = MainPage(browser, link)
+    main_page.open()
+    login_page = main_page.go_to_login_page()
+    login_page.should_be_login_page()
+    time.sleep(5)
 
 
 def test_guest_should_see_login_link(browser):
