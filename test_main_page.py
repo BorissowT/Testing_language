df84c78ced21bc0d0b46935bbf25dab9f5c102ed
tbs99 <timsregisters@gmail.com>
2020-08-25 14:18:48 +0300

    changes

--- test_main_page.py
+++ test_main_page.py
@@ -1,14 +1,14 @@
 import time
 
+link = "http://selenium1py.pythonanywhere.com/"
 
-class TestLanguage:
-    def test_if_basket_exist(self, browser):
-        link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-        browser.get(link)
-        basket = None
-        try:
-            basket = browser.find_element_by_css_selector(".btn-add-to-basket")
-        finally:
-            assert basket, "basket not found"
-        time.sleep(5)
 
+def go_to_login_page(browser):
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
+    time.sleep(5)
