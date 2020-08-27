e1aa69aa5ec830ee33bee193c620c0f73593dc3e
tbs99 <timsregisters@gmail.com>
2020-08-27 14:11:54 +0300

    Classes for pytest

--- test_main_page.py
+++ test_main_page.py
@@ -2,33 +2,34 @@ import time
 import pytest
 from .pages.main_page import MainPage
 from .pages.basket_page import BasketPage
-from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
+##to start this class
+@pytest.mark.login_guest
+class TestLoginFromMainPage():
+    def test_guest_can_go_to_login_page(self, browser):
+        main_page = MainPage(browser, link)
+        main_page.open()
+        main_page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)
+        login_page.should_be_login_page()
+        time.sleep(5)
+
+    def test_guest_should_see_login_link(self, browser):
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
+
 
 @pytest.mark.skip
-def test_guest_can_go_to_login_page(browser):
-    main_page = MainPage(browser, link)
-    main_page.open()
-    login_page = main_page.go_to_login_page()
-    login_page.should_be_login_page()
-    time.sleep(5)
-
-
-@pytest.mark.skip
-def test_guest_should_see_login_link(browser):
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
-
-
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/"
     main_page = MainPage(browser, link)
     main_page.open()
     main_page.go_to_basket()
-    basket_page = BasketPage(browser, link)
+    basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty()
 
 
--- test_product_page.py
+++ test_product_page.py
@@ -73,7 +73,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_login_page()
-    login_page = LoginPage(browser, link)
+    login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
 
@@ -82,7 +82,7 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_basket()
-    basket_page = BasketPage(browser, link)
+    basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_empty()
 
 
